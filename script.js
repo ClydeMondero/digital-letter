788620343ccfe1354398882f77481fb64f014948
Clyde Mondero <clydereinyelmondero0324@gmail.com>
2024-02-09 23:56:34 +0800

    feat: polished envelopes animation and responsiveness

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-$(".heart").on("click", function () {
-  $(this).css("opacity", "0");
-  $(".top").css("transform", "rotateX(180deg)");
-  $(".content").addClass("open");
+$(".top").click(function (e) {
+  e.preventDefault();
+
+  $(this).css("transform", "rotateX(180deg)");
+  setTimeout(function () {
+    $(".content").addClass("open");
+  }, 1000);
 
   setTimeout(function () {
     $(".top").css("opacity", "0");
