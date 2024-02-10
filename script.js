dc61ccafed64edc35f7b15f3f05b00c44fd1ad05
Clyde Mondero <clydereinyelmondero0324@gmail.com>
2024-02-10 22:46:58 +0800

    feat: book animation

--- script.js
+++ script.js
@@ -9,4 +9,26 @@ $(".top").click(function (e) {
   setTimeout(function () {
     $(".top").css("opacity", "0");
   }, 3000);
+
+  setTimeout(function () {
+    $(".content").css({
+      opacity: "0",
+      transition: "opacity 2s linear",
+    });
+
+    $(".body").css({
+      opacity: "0",
+      transition: "opacity 2s linear",
+    });
+  }, 10000);
+
+  setTimeout(function () {
+    $(".body").css("display", "none");
+    $(".content").css("display", "none");
+
+    $(".book").css({
+      display: "block",
+      opacity: "1",
+    });
+  }, 12000);
 });
